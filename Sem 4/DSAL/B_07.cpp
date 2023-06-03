afcb56478059dd0eea42dc92cedb7e1b57eed7d4
shreyapandekar <113656443+shreyapandekar@users.noreply.github.com>
2023-06-03 21:39:08 +0530

    Update B_07.cpp

--- Sem 4/DSAL/B_07.cpp
+++ Sem 4/DSAL/B_07.cpp
@@ -2,68 +2,133 @@
 traverse it using postordertraversal(non recursive) and then delete the entire tree.*/ 
 
 #include <iostream>
-#include <stack>
+#include <string.h>
 using namespace std;
 
-struct Node {
+struct node
+{
     char data;
-    Node* left;
-    Node* right;
+    node *left;
+    node *right;
 };
+class tree
+{
+    char prefix[20];
 
-Node* newNode(char data) {
-    Node* node = new Node;
-    node->data = data;
-    node->left = node->right = NULL;
-    return node;
-}
-// Function to construct the expression tree from the prefix expression
-Node* constructTree(string prefix) {
-    stack<Node*> st;
+public:
+    node *top;
+    void expression(char[]);
+    void display(node *);
+    void non_rec_postorder(node *);
+    void del(node *);
+};
+class stack1
+{
+    node *data[30];
+    int top;
 
-    for (int i = prefix.length() - 1; i >= 0; i--) {
-        char c = prefix[i];
-
-        if (isalnum(c)) {
-            st.push(newNode(c));
+public:
+    stack1()
+    {
+        top = -1;
+    }
+    int empty()
+    {
+        if (top == -1)
+            return 1;
+        return 0;
+    }
+    void push(node *p)
+    {
+        data[++top] = p;
+    }
+    node *pop()
+    {
+        return (data[top--]);
+    }
+};
+void tree::expression(char prefix[])
+{
+    char c;
+    stack1 s;
+    node *t1, *t2;
+    int len, i;
+    len = strlen(prefix);
+    for (i = len - 1; i >= 0; i--)
+    {
+        top = new node;
+        top->left = NULL;
+        top->right = NULL;
+        if (isalpha(prefix[i]))
+        {
+            top->data = prefix[i];
+            s.push(top);
         }
-        else {
-            Node* node = newNode(c);
-            node->left = st.top();
-            st.pop();
-            node->right = st.top();
-            st.pop();
-            st.push(node);
+        else if (prefix[i] == '+' || prefix[i] == '*' || prefix[i] == '-' || prefix[i] == '/')
+        {
+            t2 = s.pop();
+            t1 = s.pop();
+            top->data = prefix[i];
+            top->left = t2;
+            top->right = t1;
+            s.push(top);
         }
     }
-    return st.top();
+    top = s.pop();
 }
-// Function to perform postorder traversal of the expression tree
-void postorder(Node* node) {
-    if (node) {
-        postorder(node->left);
-        postorder(node->right);
-        cout << node->data;
+void tree::display(node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data;
+        display(root->left);
+        display(root->right);
     }
 }
-
-//Function to delete the entire expression tree
-void deleteTree(Node* node) {
-    if (node) {
-        deleteTree(node->left);
-        deleteTree(node->right);
-        delete node;
+void tree::non_rec_postorder(node *top)
+{
+    stack1 s1, s2; /*stack s1 is being used for flag . A NULL data implies that the right subtree has not been visited */
+    node *T = top;
+    cout << "\n";
+    s1.push(T);
+    while (!s1.empty())
+    {
+        T = s1.pop();
+        s2.push(T);
+        if (T->left != NULL)
+            s1.push(T->left);
+        if (T->right != NULL)
+            s1.push(T->right);
+    }
+    while (!s2.empty())
+    {
+        top = s2.pop();
+        cout << top->data;
     }
 }
+void tree::del(node *node)
+{
+    if (node == NULL)
+        return;
+    /* first delete both subtrees */
+    del(node->left);
+    del(node->right);
+    /* then delete the node */
+    cout <<endl<<"Deleting node : " << node->data<<endl;
+    free(node);
+}
+int main()
+{
+    char expr[20];
+    tree t;
 
-int main() {
-    //string prefix;
-    //cout<<"Enter the string:-";
-    //cin>>prefix;
-    string prefix = "+--a*bc/def";
-    Node* root = constructTree(prefix);
-    cout << "Postorder traversal of the expression tree: ";
-    postorder(root);
-    //deleteTree(root);
-    return 0;
+    cout <<"Enter prefix Expression : ";
+    cin >> expr;
+    cout << expr;
+    t.expression(expr);
+    //t.display(t.top);
+    //cout<<endl;
+    t.non_rec_postorder(t.top);
+    t.del(t.top);
+    // t.display(t.top);
 }
